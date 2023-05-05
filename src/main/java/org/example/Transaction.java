22241726906eabb35ecfe78a7885bb52ce4ada1e
tchou0 <129346770+tchou0@users.noreply.github.com>
2023-05-05 16:12:56 -0400

    Update Transaction.java

--- src/main/java/org/example/Transaction.java
+++ src/main/java/org/example/Transaction.java
@@ -2,27 +2,15 @@ package org.example;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-/* 
- * Create a Transaction.java file with different variables to make getters and setters and a constructor so we can use in our other classes
- * 
- * SETTERS: we use setters to modify (change) the value of the private field
- * GETTERS: we use getters to access (get data) the value of the private field
- * 
- * Constructors: We use the private variables we use as arguements (what we put in parameters for our constructors) 
-*/
-
-//==================================== PROPERTIES =================================
-
+//================================= PROPERTIES ===================================
 public class Transaction {
     private LocalDate date;
-    private LocalTime time;                     //my properties to return the information 
+    private LocalTime time;                     // my properties to return the information
     private String description;
     private String vendor;
     private double amount;
 
-//============================ CONSTRUCTOR ========================================
-
-
+//=============================== CONSTRUCTOR ========================================
     //our constructor that we can call on other classes, with our parameters
     public Transaction(LocalDate date, LocalTime time, String description, String vendor, double amount) {
         this.date = date;
@@ -31,29 +19,22 @@ public class Transaction {
         this.vendor = vendor;
         this.amount = amount;
     }
-
-//================================== GETTERS =======================================
-
-
+//================================== GETTERS ========================================
     public LocalDate getDate() {    //return the date (access data)
         return date;
     }
-
 
     public LocalTime getTime() {    //returns the time (access time)
         return time;
     }
 
-
     public String getDescription() {    //return the description (access description)
         return description;
     }
 
-
     public String getVendor() {     //return the vendor (access vendor)
         return vendor;
     }
-
 
     public double getAmount() {     //return the amount (access amount)
         return amount;
